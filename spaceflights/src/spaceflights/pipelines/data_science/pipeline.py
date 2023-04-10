ea39fd08756ca3704617552872bbd5581b23c38f
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2023-04-10 17:12:05 +0100

    Adding namespacing

--- spaceflights/src/spaceflights/pipelines/data_science/pipeline.py
+++ spaceflights/src/spaceflights/pipelines/data_science/pipeline.py
@@ -1,10 +1,11 @@
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
 from .nodes import evaluate_model, split_data, train_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
+    pipeline_instance =  pipeline(
         [
             node(
                 func=split_data,
@@ -26,3 +27,16 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
         ]
     )
+
+    ds_pipeline_1 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="active_modelling_pipeline",
+    )
+    ds_pipeline_2 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="candidate_modelling_pipeline",
+    )
+
+    return ds_pipeline_1 + ds_pipeline_2
